39a1a68f614df9b4be1034f5bca3c2b6c2275d4c
Kokaa Leal <calhau.bruno88@gmail.com>
2019-05-09 23:55:01 -0300

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -20,8 +23,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
-    
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
