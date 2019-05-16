4ee313fb63cb7a3a34059ff659684c7e0806f7f9
Kokaa Leal <calhau.bruno88@gmail.com>
2019-05-15 22:30:59 -0300

    Week 2 - Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FlexLayoutModule } from '@angular/flex-layout'
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 //Material Design Modules
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from '@angular/material/toolbar'
-
 //Components
 import { AppComponent } from './app.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { MenuComponent } from './menu/menu.component';
-
-import 'hammerjs';
-
-//services
-import { DishService } from './services/dish.service';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
+import 'hammerjs';
+
+//services
+import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -51,7 +48,7 @@ import { PromotionService } from './services/promotion.service';
     MatCardModule,
     MatButtonModule    
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, LeaderService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
