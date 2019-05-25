6760aa4a8ea374febb41b987c54666964f1de3a7
Kokaa Leal <calhau.bruno88@gmail.com>
2019-05-25 07:50:40 -0300

    Week 2 - Ex 2 - Templates Form PART 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FlexLayoutModule } from '@angular/flex-layout'
 import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
+//Import Modules
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from "@angular/forms";
 //Material Design Modules
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatGridListModule } from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from '@angular/material/toolbar'
 //Components
 import { AppComponent } from './app.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { MenuComponent } from './menu/menu.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 import 'hammerjs';
 
@@ -25,6 +34,7 @@ import 'hammerjs';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+
 
 @NgModule({
   declarations: [
@@ -35,18 +45,27 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     FlexLayoutModule,
-    MatToolbarModule,
-    MatListModule,
-    MatGridListModule,
+    MatButtonModule,    
     MatCardModule,
-    MatButtonModule    
+    MatCheckboxModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
+  entryComponents: [
+    LoginComponent,
   ],
   providers: [DishService, LeaderService, PromotionService],
   bootstrap: [AppComponent]
